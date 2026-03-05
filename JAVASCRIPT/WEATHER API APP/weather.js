82e77236fd4b514e6955b380247b7e35796dbb8c
PikachuBoss69 <anujnegi7367@gmail.com>
2026-03-05 13:50:29 +0530

    pending

---
+++ JAVASCRIPT/WEATHER API APP/weather.js
@@ -0,0 +1,11 @@
+const input = document.getElementById("input-block");
+const apiKey = "";
+function getWeatherDetails(){
+
+}
+function updateWeatherImages(){
+
+}
+function updateWeatherDetails(){
+    
+}
