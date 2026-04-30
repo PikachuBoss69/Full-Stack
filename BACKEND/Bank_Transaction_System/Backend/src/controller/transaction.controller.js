95e72af4031fbc643de37c324488d2bffdf8bfc4
PikachuBoss69 <anujnegi7367@gmail.com>
2026-04-30 18:51:35 +0530

    completed createTransaction function

--- BACKEND/Bank_Transaction_System/Backend/src/controller/transaction.controller.js
+++ BACKEND/Bank_Transaction_System/Backend/src/controller/transaction.controller.js
@@ -3,6 +3,8 @@ const ledgerModel = require("../models/ledger.model")
 const accountModel = require("../models/account.model")
 const emailService = require("../services/email.service")
 const mongoose = require("mongoose")
+
+//Created to create custom errors full production level
 const AppError = require("../utils/AppError");
 
 async function createTransaction(req, res){
@@ -13,16 +15,58 @@ async function createTransaction(req, res){
         
         const { fromUserAccount, toUserAccount} = await validateRequest(fromAccount, toAccount, amount, idempotencyKey);
 
-            const transaction = await isTransactionAlreadyExist(idempotencyKey);
+            const existing_transaction = await isTransactionAlreadyExist(idempotencyKey);
 
-        //Used optional cahining here , for my own reminder as im forgetful about small things
-        if (transaction?.type === "ALREADY_COMPLETED") {
+        //Used optional chaning here , for my own reminder as im forgetful about small things
+        if (existing_transaction?.type === "ALREADY_COMPLETED") {
             return res.status(200).json({
                 message: "Transaction already processed",
-                transaction: result.transaction
+                transaction: existing_transaction.transaction
             });
         }
+        if (existing_transaction?.type === "PENDING") {
+            return res.status(200).json({
+                message: "Transaction is still processing",
+            });
+        }
+                        
         await checkAccountStatus(fromUserAccount, toUserAccount);
+
+        await validateUserAccountBalance(fromUserAccount, amount);
+
+        const session = await mongoose.startSession();
+        let transaction;
+        try {
+            session.startTransaction();
+
+            transaction = await executeTransactionFlow({
+                fromAccount,
+                toAccount,
+                amount,
+                idempotencyKey,
+                session
+            });
+
+            await session.commitTransaction();
+            session.endSession();
+
+        } catch (err) {
+            await session.abortTransaction();
+            session.endSession();
+            throw err;
+        }
+        sendTransactionEmail({
+            _email:req.user.email,
+            _name:req.user.name,
+            _amount:amount,
+            _toAccount:toAccount
+        }).catch(console.error);
+        
+        return res.status(201).json({
+        message: "Transaction completed successfully",
+        transaction: transaction
+        })
+
     }catch(err){
         res.status(err.status || 400).json({
              message: err.message
@@ -34,7 +78,7 @@ async function createTransaction(req, res){
 
 async function validateRequest(fromAccount, toAccount, amount, idempotencyKey){
     if(!fromAccount || !toAccount || !amount || !idempotencyKey){
-       throw new Error("Missing required parameters");
+       throw new AppError("Missing required parameters", 400);
     }
 
     const fromUserAccount = await accountModel.findOne({
@@ -46,7 +90,7 @@ async function validateRequest(fromAccount, toAccount, amount, idempotencyKey){
     })
 
     if(!fromUserAccount || !toUserAccount){
-        throw new Error("Invalid fromAccount or toAccount");
+        throw new AppError("Invalid fromAccount or toAccount", 400);
     }
     return { fromUserAccount, toUserAccount };
 
@@ -67,9 +111,10 @@ async function isTransactionAlreadyExist(idempotencyKey){
     }
 
     if (existingTransaction.status === "PENDING") {
-            
-        throw new AppError("Transaction is still processing", 200);
-  
+
+        return {
+            type: "PENDING",
+        };
     }
 
     if (existingTransaction.status === "FAILED") {
@@ -87,7 +132,63 @@ async function isTransactionAlreadyExist(idempotencyKey){
 async function checkAccountStatus(fromUserAccount, toUserAccount){
      if (fromUserAccount.status !== "ACTIVE" || toUserAccount.status !== "ACTIVE") {
        
-        throw new Error("Both fromAccount and toAccount must be ACTIVE to process transaction");
+        throw new AppError("Both fromAccount and toAccount must be ACTIVE to process transaction", 400);
 
     }
 }
+
+async function validateUserAccountBalance(fromUserAccount, amount){
+    const _balance = await fromUserAccount.getBalance();
+    
+    if(_balance < amount){
+        throw new AppError( `Insufficient balance. Current balance is ${_balance}. Requested amount is ${amount}`,400)
+    }
+}
+
+async function executeTransactionFlow({fromAccount, toAccount, amount, idempotencyKey, session}){
+    //We have done destructing here , we could have used array indexing , but just it look much dashing and less complex
+    const [_transaction] = await transactionModel.create([ {
+            fromAccount,
+            toAccount,
+            amount,
+            idempotencyKey,
+            status: "PENDING"
+        } ], { session });
+    
+    await createDebitLedgerEntry({fromAccount, amount, _transaction, session});
+    await createCreditLedgerEntry({toAccount, amount, _transaction, session});
+        
+    await transactionModel.findOneAndUpdate(
+            { _id: _transaction._id },
+            { status: "COMPLETED" },
+            { session }
+        )
+    return _transaction;
+}
+
+async function createDebitLedgerEntry({fromAccount, amount, _transaction, session}){
+      await ledgerModel.create([ {
+            account: fromAccount,
+            amount: amount,
+            transaction: _transaction._id,
+            type: "DEBIT"
+        } ], { session })
+}
+
+async function createCreditLedgerEntry({toAccount, amount, _transaction, session}){
+    await ledgerModel.create([ {
+            account: toAccount,
+            amount: amount,
+            transaction: _transaction._id,
+            type: "CREDIT"
+        } ], { session })
+}
+
+async function sendTransactionEmail({
+        _email, _name, _amount, _toAccount
+    }){
+        await emailService.sendTransactionEmail(_email, _name, _amount ,_toAccount);
+        
+}
+
+module.exports = {createTransaction}
