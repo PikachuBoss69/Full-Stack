3c4172ecae0383a7596fe68304534101d62e705a
PikachuBoss69 <anujnegi7367@gmail.com>
2026-05-03 11:36:25 +0530

    adding details

---
+++ TYPESCRIPT/typescript1/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(name) {
+    return `hello ${name}`;
+}
+const user = "naman";
+console.log(hello(user));
+//# sourceMappingURL=index.js.map
---
+++ TYPESCRIPT/typescript1/src/index.ts
@@ -0,0 +1,7 @@
+function hello(name:string):string{
+    return `hello ${name}`
+}
+
+const user:string = "naman"
+
+console.log(hello(user))
